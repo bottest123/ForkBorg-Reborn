f2864b220557e69015f88f2262875167c3e2e3db
bottest123 <41206183+bottest123@users.noreply.github.com>
2018-10-17 23:59:35 +0530

    Edit it and save and run the bot

---
+++ config.py
@@ -0,0 +1,19 @@
+API_ID="Your App ID"
+API_HASH="Your API HASH"
+PRIVATE_CHANNEL_BOT_API_ID="USE .get_id to get from channel"              #For using .fwd command to forward a message directly without quotes to this specified channel
+PRIVATE_GROUP_BOT_API_ID="use .get_id "                #For using .fwd command to forward a message directly to a group or channel
+SCREEN_SHOT_LAYER_ACCESS_KEY="get from screenshot layer website google it "           #For using .screencapture commad...please refer readme for getting the key
+OPEN_WEATHER_MAP_APPID="get it from openweather site"        #FOR USING .weather Command
+IBM_WATSON_CRED_USERNAME="INSERT API USERNAME" #FOR IBM WATSON AUDIO USAGE
+IBM_WATSON_CRED_PASSWORD="INSERT API PASSWORD"        #For IBM WATSON 
+LOGGER_GROUP="CHAT ID (-ve) OF THE LOG GROUP"
+LOGGER=False    #Incase you want to turn off logging, put this to false
+CONSOLE_LOGGER_VERBOSE=False
+PM_AUTO_BAN=False
+#DONT FILL IT YET NOT NECESSARY
+TRT_ENABLE=False
+TTS_ENABLE=False
+TRT_API_USERNAME="Insert API Username"    #For Using IBM Translator API
+TTS_API_USERNAME="Insert API Username"    #For IBM Voice API
+TRT_API_PASSWORD="Insert API Password"
+TTS_API_PASSWORD="Insert API Password"
