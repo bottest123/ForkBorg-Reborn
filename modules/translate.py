c195d2a42218ca0437e92f00714e912e8a831073
bottest123 <41206183+bottest123@users.noreply.github.com>
2018-10-21 18:29:52 +0530

    Updates 😁

---
+++ modules/translate.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+import json
+from py_translator import Translator
+
+
+@bot.on(events.NewMessage(pattern=r"\.tr (.*)", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    start = datetime.now()
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        text = previous_message.message
+        lan = input_str
+    elif "|" in input_str:
+        lan, text = input_str.split("|")
+    else:
+        await event.edit("Invalid Syntax. Module stopping.")
+        return
+    translator = Translator()
+    try:
+        translated = translator.translate(text, dest=lan)
+        src_lang = translated.src
+        translated_text = translated.text
+        end = datetime.now()
+        ms = (end - start).seconds
+        output_str = "Translated from {} to {} in {} seconds. \n {}".format(src_lang, lan, str(ms), translated_text)
+        await event.edit(output_str)
+    except exc:
+        await event.edit(str(exc))
---
+++ modules/wikipedia.py
@@ -0,0 +1,15 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import wikipedia
+
+
+@bot.on(events.NewMessage(pattern=r"\.wikipedia (.*)", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    await event.edit("Processing ...")
+    input_str = event.pattern_match.group(1)
+    result = wikipedia.summary(input_str)
+    await event.edit("**Search**: {} \n\n **Result**: \n\n {}".format(input_str, result))
